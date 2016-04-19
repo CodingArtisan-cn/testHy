53c67a38ae41278bb8759f322dfcb4ac96688ca8
huangyong <huangyongwr@live.com>
2016-04-19 10:23:55 +0800

    just a test

---
+++ hy.cpp
@@ -0,0 +1,17 @@
+/*************************************************************************
+	> File Name: hy.cpp
+  > Description: 
+	> Author: HuangChunLin
+	> Mail:huangyongwr@live.com
+	> Created Time: Tue 19 Apr 2016 10:23:15 AM CST
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+ 
+int main()
+{
+    cout<<"just a test"<<endl;
+    return 0;
+}
+
